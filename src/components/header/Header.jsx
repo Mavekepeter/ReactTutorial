6bbe70ac8e64f4afbc129042e9c7c8015e987f43
samuel maveke <mavekepeter2022@gmail.com>
2024-11-13 18:15:37 +0300

    state management in react

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import ReactLogo from "../../assets/react.svg"
+import NavItems from "../navitems/NavItems"
+
+const Header = (props) => {
+  return (
+    <div className="h-[50px w-full py-[0.5rem] px-[1rem] border-b border-neutral-300 mb-[1rem] flex justify-between items-center">
+    <div>
+        <img src={ReactLogo}alt="" className="w-[30px] h-30px]"/>
+    </div>
+    
+    <div>
+       <NavItems navItems={props.navItems}/>
+    </div>
+    <button onClick={props.increaseCounter} className="py-1 px-3 bg-blue-500 text-white mr-1">Increament</button>
+    <button onClick={props.decreaseCounter} className="py-1 px-3 bg-blue-500 text-white">Decrement</button>
+
+    <div className="flex items-center ">
+         <img src={ReactLogo} alt="" className="w-[30px] h-[30px] mr-[10px]" />
+         <p>{name}</p>
+    </div>
+    </div>
+  )
+}
+
+export default Header
